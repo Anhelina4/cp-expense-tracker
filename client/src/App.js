0291656661e359735734773f3562312c0412d73b
Anhelina Bohdanova <gelyabogdanova88@gmail.com>
2023-05-02 09:25:36 +0300

    added some tailwindcss classes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 function App() {
   return (
-    <div>
-      <h1>Expense tracker</h1>
+    <div className="container text-center drop-shadow-lg text-grey-800">
+      <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">
+        Expense tracker
+      </h1>
+
+      {/* GRID COLUMNS */}
+      <div className="grid md:grid-cols-2 gap-4"></div>
     </div>
   );
 }
